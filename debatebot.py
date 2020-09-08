4e39ef79bc2460982481408902964473ae22485b
thesuhas <suhastvs@gmail.com>
2020-09-09 00:25:43 +0530

    Added basic commands

--- debatebot.py
+++ debatebot.py
@@ -5,20 +5,117 @@ import asyncio
 client = commands.Bot(command_prefix='.')
 client.remove_command('help')
 
+# Creating a debate class
+class Debate:
+    def __init__(self, status=False, format="NA", reply="NA", speaker='PM'):
+        self.status = status
+        self.format = format
+        self.reply = reply
+        self.speaker = speaker
+
 @client.event
 async def on_ready():
     print("Bot is ready")
+    global deb
+    global speakers
+    deb = Debate()
+    speakers = {'BP': ['PM', 'LO', 'DPM', 'DLO', 'MG', 'OG', 'GW', 'OW'], 'AP': ['PM', 'LO', 'DPM', 'DLO', 'GW', 'OW']}
 
 @client.command(aliases=['h', "help"])
 async def _help(ctx):
     author = ctx.message.author
     help_e = discord.Embed(title='Debate Time Keeper Bot', color=discord.Color.green())
     name = ".start to start a debate\n.format {format} to set the debate format\n"
-    format = "Formats are AP and BP\nIf AP, .reply to set if reply (Y/N)\n"
+    format = "Formats are AP and BP\n"
     time = "Speaker time is 7:20 per person"
     help_e.add_field(name='Description', value=name, inline=False)
     help_e.add_field(name='Formats', value=format)
     help_e.add_field(name="Speaker Time", value=time)
     await author.send(embed=help_e)
+
+@client.command()
+async def start(ctx):
+    global deb
+    if deb.status == False:
+        deb.status = True
+        await ctx.send(f"Debate has been started by {ctx.author.mention}")
+        asyncio.sleep(1)
+        await ctx.send("Please set the format with `.format {format}`")
+    elif deb.status == True:
+        await ctx.send("A debate is already going on")    
+
+@client.command()
+async def format(ctx, format):
+    global deb
+    # If debate has started
+    if deb.status == True:
+        # If format has not been set yet
+        if deb.format == 'NA':
+            deb.format = format
+            await ctx.send(f"Debate format set to {format} by {ctx.author.mention}")
+        else:
+            await ctx.send(f"Debate format has already been set to {deb.format}")
+    else:
+        await ctx.send("Debate has not started yet. Please start with `.start`")
+
+@client.command()
+async def end(ctx):
+    global deb
+    # If debate has not ended yet
+    if deb.status == True:
+        # Resets all parameters
+        deb.status = False
+        deb.format = 'NA'
+        deb.reply = 'NA'
+        deb.speaker = 'PM'
+        await ctx.send(f"Debate has been ended by {ctx.author.mention}")
+    else:
+        await ctx.send("No debate going on")
+
+@client.command()
+async def speech(ctx):
+    global deb
+    global speakers
+    # If debate has started
+    if (deb.status == True):
+        # If format has been set
+        if (deb.format != 'NA'):
+            await ctx.send(f"I now invite the {deb.speaker} for their speech")
+            index = speakers[deb.format].index(deb.speaker)
+            await asyncio.sleep(5)
+            await ctx.send("Protected time is done")
+            await asyncio.sleep(5)
+            await ctx.send("Protected time has started")
+            await asyncio.sleep(5)
+            await ctx.send("Time up!")
+            # If it is not the last speaker
+            if index != len(speakers[deb.format]) - 1:
+                deb.speaker = speakers[deb.format][index + 1]
+            elif index == len(speakers[deb.format]) - 1:
+                deb.status = False
+                deb.speaker = 'PM'
+                deb.format = 'NA'
+                deb.reply = 'NA'
+                await ctx.send("The debate has finished")
+        else:
+            await ctx.send("The format has not been set. Please set it with `.format`")
+    else:
+        await ctx.send("The debate has not started. Please set it with `.start`")
+
+@client.command()
+async def status(ctx):
+    global deb
+    if deb.status == True:
+        embed = discord.Embed(title = 'Debate Status', colour = discord.Colour.blue(), description="Started")
+        if deb.format != 'NA':
+            embed.add_field(name='Format', value=deb.format)
+            embed.add_field(name='Speaker', value=deb.speaker)
+        else:
+            embed.add_field(name='Format', value='To Be Set')
+        await ctx.message.author.send(embed=embed)
+    else:
+        embed = discord.Embed(title='Debate Status', colour=discord.Colour.red(), description='Not going on')
+        await ctx.message.author.send(embed=embed)
+
 
 client.run('NzUyODg5MzI2Mzc0ODc5MjU2.X1eM0w.7-oN3tzEguiGTDxfsyYNr6p53sU')
